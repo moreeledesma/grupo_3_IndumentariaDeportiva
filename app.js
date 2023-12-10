545461ce7ed98b8b1af8502ffbfa2d09e1758548
gabrielcasali <gabriel9casali@gmail.com>
2023-12-10 19:30:51 -0300

    cambios-gabi

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicPath = path.resolve("public");
-app.use(express.static(publicPath));
 
+app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
     res.sendFile(path.resolve("./views/index.html"));
-});
+})
 
 app.get("/registro", (req, res) => {
     res.sendFile(path.resolve("./views/registro.html"));
-});
+})
 
 app.get("/login", (req, res) => {
     res.sendFile(path.resolve("./views/login.html"));
-});
+})
 
 app.get("/carrito", (req, res) => {
     res.sendFile(path.resolve("./views/carrito.html"));
-});
+})
 
 app.listen(3030, ()=>{
-    console.log("Servidor corriendo");
-});
+    console.log(`Servidor funcionando`);
+})
